70efc066d8c1d41da8ceef8493deb7ce8f0d2f8a
sudeep27 <sudeepap102@gmail.com>
2026-05-08 07:54:31 +0000

    professional portfolio UI upgrade

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
        path('', views.home),
        path('projects/', views.projects),
-       path('contacts/', views.contact),
+       path('contact/', views.contact),
        path('about/', views.about),
        ]
